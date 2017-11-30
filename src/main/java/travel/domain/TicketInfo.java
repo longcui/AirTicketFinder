a929123ca550b5a06c13908f26a963b7ee050a24
long <long@origin.no>
2017-11-30 14:28:59 +0100

    add support for adding command properties

---
+++ src/main/java/travel/domain/TicketInfo.java
@@ -0,0 +1,38 @@
+package travel.domain;
+
+public class TicketInfo {
+    private double cheapest = Double.MAX_VALUE, quickest = Double.MAX_VALUE, best = Double.MAX_VALUE;
+
+    public double getCheapest() {
+        return cheapest;
+    }
+
+    public void setCheapest(double cheapest) {
+        this.cheapest = cheapest;
+    }
+
+    public double getQuickest() {
+        return quickest;
+    }
+
+    public void setQuickest(double quickest) {
+        this.quickest = quickest;
+    }
+
+    public double getBest() {
+        return best;
+    }
+
+    public void setBest(double best) {
+        this.best = best;
+    }
+
+    @Override
+    public String toString() {
+        return "TicketInfo{" +
+                "cheapest=" + cheapest +
+                ", quickest=" + quickest +
+                ", best=" + best +
+                '}';
+    }
+}
---
+++ src/main/java/travel/finnAndMomodo/Travel.java
@@ -0,0 +1,43 @@
+package travel.finnAndMomodo;
+
+import travel.domain.TicketInfo;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Travel {
+
+    protected static boolean enableEmailNotification = true;
+
+
+    protected static List<String> fromChinaCities = new ArrayList<String>();
+    protected static List<String> toNorwayCities = new ArrayList<String>();
+    protected static List<String> fromNorwayCities = new ArrayList<String>();
+    protected static List<String> toChinaCities = new ArrayList<String>();
+
+    protected static List<String> fromDates = new ArrayList<String>();
+    protected static List<String> toDates = new ArrayList<String>();
+
+    protected static List<TicketInfo> prices = new ArrayList<TicketInfo>();
+    protected static List<String> bestPriceUrls = new ArrayList<String>();
+
+    protected static SimpleDateFormat sdfMo = new SimpleDateFormat("dd-M-YYYY");     //14-5-2015
+    protected static SimpleDateFormat sdfFin = new SimpleDateFormat("dd.MM.YYYY");     //14.05.2015
+
+
+
+    protected static void prepareWritingToExcel(String fromChinaCity, String toNorwayCity, String fromNorwayCity, String toChinaCity, Date from, Date to, TicketInfo price, String bestPriceURL) {
+        fromChinaCities.add(fromChinaCity);
+        toNorwayCities.add(toNorwayCity);
+        fromNorwayCities.add(fromNorwayCity);
+        toChinaCities.add(toChinaCity);
+        fromDates.add(sdfFin.format(from));
+        if(to != null) {
+            toDates.add(sdfFin.format(to));
+        }
+        prices.add(price);
+        bestPriceUrls.add(bestPriceURL);
+    }
+}
