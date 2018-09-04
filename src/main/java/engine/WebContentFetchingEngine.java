8ba3e6e1495f92a152ba48a47bf4cf8f99e3f421
long <long@origin.no>
2018-09-04 10:01:58 +0200

    refactor

--- src/main/java/engine/WebContentFetchingEngine.java
+++ src/main/java/engine/WebContentFetchingEngine.java
@@ -1,27 +1,12 @@
 package engine;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import travel.browser.SeleniumWebDriverFirefox;
-
-import java.util.List;
 
 public class WebContentFetchingEngine {
     private static WebDriver driver = SeleniumWebDriverFirefox.getDriver();
 
-    public static boolean isProductExist(String productUrl) {
-        WebDriverWait wait = new WebDriverWait(driver, 30);
-        driver.get(productUrl);
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("available--now")));
-
-        By className = By.className("available--now");
-        List<WebElement> element = driver.findElements(className);
-        if(element.size() != 0) {
-            return true;
-        } else {
-            return false;
-        }
+    public static WebDriver getWebDriver() {
+        return driver;
     }
 }
--- src/main/java/website/lego/LegoProductFinder.java
+++ src/main/java/website/lego/LegoProductFinder.java
@@ -1,10 +1,15 @@
 package website.lego;
 
 import engine.WebContentFetchingEngine;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import travel.Credential;
 import utils.EmailUtils;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 public class LegoProductFinder {
@@ -13,12 +18,20 @@ public class LegoProductFinder {
         Random random = new Random();
 
         while (true) {
+            String url = "https://shop.lego.com/en-NO/Millennium-Falcon-75192";
 //        boolean isProductExist = WebContentFetchingEngine.isProductExist("https://shop.lego.com/en-NO/Downtown-Diner-10260");
-            boolean isProductExist = WebContentFetchingEngine.isProductExist("https://shop.lego.com/en-NO/Millennium-Falcon-75192");
+            WebDriver webDriver = WebContentFetchingEngine.getWebDriver();
+            WebDriverWait wait = new WebDriverWait(webDriver, 30);
+            webDriver.get(url);
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("available--now")));
+
+            By className = By.className("available--now");
+            List<WebElement> element = webDriver.findElements(className);
+            boolean isProductExist = element.size() != 0;
             System.out.println(new Date() + ": " + isProductExist);
 
             if(isProductExist) {
-                EmailUtils.sendEmail(Credential.EMAILS[0], "Millennium Falcon Product is available now", "https://shop.lego.com/en-NO/Millennium-Falcon-75192");
+                EmailUtils.sendEmail(Credential.EMAILS[0], "Millennium Falcon Product is available now", url);
                 break;
             }
 //            EmailUtils.sendEmail("longcuino@gmail.com", "Millennium Falcon Product is available now", "https://shop.lego.com/en-NO/Millennium-Falcon-75192");
