a725f7b9bcbc7b29cf2d4acc53b90a77f08aed4e
long <long@origin.no>
2018-11-15 09:27:37 +0100

    update to new version of selenium(old one does not work)

--- src/main/java/travel/browser/SeleniumWebDriverFirefox.java
+++ src/main/java/travel/browser/SeleniumWebDriverFirefox.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumWebDriverFirefox {
     public static FirefoxDriver getDriver() {
-        System.setProperty("webdriver.gecko.driver", "C:\\programs\\geckodriver-v0.19.1-win64\\geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", "C:\\programs\\geckodriver-v0.23.0-win64\\geckodriver.exe");
 
         https://stackoverflow.com/questions/37803781/disable-log-trace-in-marionette-driver
         System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE,"true");
