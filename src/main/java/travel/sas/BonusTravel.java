b3bf17dcbf61827bfa11ab68a06494c9d132c323
long <long@origin.no>
2019-01-15 09:07:22 +0100

    change to BufferedReader

--- src/main/java/travel/sas/BonusTravel.java
+++ src/main/java/travel/sas/BonusTravel.java
@@ -1,17 +1,19 @@
 package travel.sas;
 
-import travel.finnAndMomodo.Travel;
 import utils.EmailUtils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
+import java.util.Random;
+import java.util.stream.Collectors;
 
-public class BonusTravel extends Travel {
+public class BonusTravel {
     public enum ChinaCityCode {
         BEIJING("PEK"),
         SHANGHAI("PVG");
@@ -24,22 +26,23 @@ public class BonusTravel extends Travel {
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        Random random = new Random();
         while (true) {
             for (ChinaCityCode chinaCityCode : ChinaCityCode.values()) {
-                LocalDate localDate = LocalDate.of(2019, 8, 13);
-                for (int i = 0; i < 19; i++) {
+                LocalDate localDate = LocalDate.of(2019, 8, 9);
+                for (int i = 0; i < 7; i++) {
                     localDate = localDate.plusDays(1);
                     String stringDate = localDate.format(DateTimeFormatter.BASIC_ISO_DATE);
-                    URL url = new URL("https://api.flysas.com/offers/flights?to=SVG&from=" + chinaCityCode.des +  "&outDate=" + stringDate + "&adt=2&chd=0&inf=0&yth=0&bookingFlow=points&pos=no&channel=web&displayType=upsell");
+                    URL url = new URL("https://api.flysas.com/offers/flights?to=SVG&from=" + chinaCityCode.des +  "&outDate=" + stringDate + "&adt=1&chd=0&inf=0&yth=0&bookingFlow=points&pos=no&channel=web&displayType=upsell");
                     URLConnection urlConnection = url.openConnection();
                     InputStream inputStream = urlConnection.getInputStream();
-                    Scanner scanner = new Scanner(inputStream);
-                    String reply = scanner.next();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                    String reply = bufferedReader.lines().collect(Collectors.joining());
                     System.out.println(chinaCityCode + ": " + stringDate + ": " + reply);
                     if (!reply.startsWith("{\"errors\":[{\"errorCode\"")) {
                         EmailUtils.sendEmail("longcuino@gmail.com", "Bonus Travel Changed: " + reply, reply);
                     }
-                    Thread.sleep(5 * 60 * 100);
+                    Thread.sleep(random.nextInt(10) * 60 * 1000);
                 }
             }
 
